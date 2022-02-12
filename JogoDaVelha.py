179e5a4bd2f094beeaf64a3e334a5ebccb219d70
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2022-02-11 22:36:32 -0300

    Jogo da velha

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -1,21 +1,15 @@
 from os import system
 from random import randint
+from dataclasses import dataclass
 
-class TicTacToe:
+@dataclass
+class Tabuleiro:
+
     O=["O", "O", "O"]
     X=["X", "X", "X"]
     linhas = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
     colunas = []
     diagonais = []
-
-    def salva_linhas(self):
-        linha=int(input("Digite em qual linha deseja jogar: "))
-        coluna=int(input("Digite em qual coluna deseja jogar: "))
-        while self.linhas[linha-1][coluna-1]!="_":
-            print("A posição já está preenchida. Por favor, selecione casa desocupada")
-            linha=int(input("Digite em qual linha deseja jogar: "))
-            coluna=int(input("Digite em qual coluna deseja jogar: "))
-        self.linhas[linha-1][coluna-1]=self.identificacao
 
     @classmethod
     def imprime_tabuleiro(cls):
@@ -23,7 +17,22 @@ class TicTacToe:
         for linha in cls.linhas:
             print(linha)
         print("\n")
-    
+
+    @classmethod
+    def checa_o_tabuleiro(cls):
+        if (cls.O in cls.linhas or cls.O in cls.colunas or cls.O in cls.diagonais):
+            return "O"
+        elif (cls.X in cls.linhas or cls.X in cls.colunas or cls.X in cls.diagonais):
+            return "X"
+        else:
+            return "next!"
+
+    @classmethod
+    def limpa_tabuleiro(cls):
+        cls.linhas=[["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
+        cls.colunas=[["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
+        cls.diagonais=[["_", "_", "_"], ["_", "_", "_"]]
+
     @classmethod
     def salva_colunas(cls):
         cls.colunas.append([cls.linhas[0][0], cls.linhas[1][0], cls.linhas[2][0]])
@@ -35,79 +44,83 @@ class TicTacToe:
         cls.diagonais.append([cls.linhas[0][0], cls.linhas[1][1], cls.linhas[2][2]])
         cls.diagonais.append([cls.linhas[0][2], cls.linhas[1][1], cls.linhas[2][0]])
 
-    @classmethod
-    def checar_empate(cls):
-        if "_" not in TicTacToe.linhas[0] and "_" not in TicTacToe.linhas[1] and "_" not in TicTacToe.linhas[2]:
-            return True
-    
-    def registrar_empate(self):
-        print(f"Empate, o placar está  {p1.quantas_vitorias()} para o jogador 1, e {p2.quantas_vitorias()} para o jogador 2\n")
+class Jogador:
 
-    @classmethod
-    def checar_vitoria(cls):
-        if (cls.O in cls.linhas or cls.O in cls.colunas or cls.O in cls.diagonais):
-            if p1.identificacao=="O":
-                p1.salva_vitoria()
-            else:
-                p2.salva_vitoria()
-            return "O"
-        elif (cls.X in cls.linhas or cls.X in cls.colunas or cls.X in cls.diagonais):
-            if p1.identificacao=="X":
-                p1.salva_vitoria()
-            else:
-                p2.salva_vitoria()
-            return "X"
-        else:
-            return "next!"
-    
-    def registrar_vitoria(self):
-        print(f"Parabéns!, o placar está  {p1.quantas_vitorias()} para o jogador 1, e {p2.quantas_vitorias()} para o jogador 2\n")
-
-    @classmethod
-    def limpa_tabuleiro(cls):
-        cls.linhas=[["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-        cls.colunas=[["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-        cls.diagonais=[["_", "_", "_"], ["_", "_", "_"]]
-
-class Jogador(TicTacToe):
     def __init__(self):
         self.identificacao=""
         self.qtd_de_vitorias=0
+       
+    def quantas_vitorias(self):
+        return self.qtd_de_vitorias
+
+    def checa_empate():
+        if "_" not in Tabuleiro.linhas[0] and "_" not in Tabuleiro.linhas[1] and "_" not in Tabuleiro.linhas[2]:
+            return True
+
+    def registra_empate(self):
+            print(f"Empate, o placar está {p1.quantas_vitorias()} para o jogador 1 e {p2.quantas_vitorias()} para o jogador 2")
+    
+    def registra_vitoria(self):
+        if Tabuleiro.checa_o_tabuleiro()=="X":
+            if p1.identificacao=="X":
+                p1.qtd_de_vitorias+=1
+            else:
+                p2.qtd_de_vitorias+=1
+        if Tabuleiro.checa_o_tabuleiro()=="O":
+            if p1.identificacao=="O":
+                p1.qtd_de_vitorias+=1
+            else:
+                p2.qtd_de_vitorias+=1
+        print(f"Parabéns, o placar está {p1.quantas_vitorias()} para o jogador 1 e {p2.quantas_vitorias()} para o jogador 2") 
+
+class Humano(Jogador):
 
     @staticmethod
-    def escolha():
-        escolha1=input("O jogador 1 será O ou X? ")
-        if escolha1=="O":
+    def identificiacao():
+        escolha=input("O jogador 1 será O ou X? ")
+        if escolha=="O":
             p1.identificacao="O"
             p2.identificacao="X"
         else:
             p1.identificacao="X"
             p2.identificacao="O"
 
-    def salva_vitoria(self):
-        self.qtd_de_vitorias+=1
-    
-    def quantas_vitorias(self):
-        return self.qtd_de_vitorias
+    def salva_linhas(self):
+        linha=int(input("Digite em qual linha deseja jogar: "))
+        coluna=int(input("Digite em qual coluna deseja jogar: "))
+        while Tabuleiro.linhas[linha-1][coluna-1]!="_":
+            print("A posição já está preenchida. Por favor, selecione casa desocupada")
+            linha=int(input("Digite em qual linha deseja jogar: "))
+            coluna=int(input("Digite em qual coluna deseja jogar: "))
+        Tabuleiro.linhas[linha-1][coluna-1]=self.identificacao
 
 class Computador(Jogador):
+
+    def identificiacao(staticmethod):
+        escolha=input("Deseja jogar com X ou O? ")
+        if escolha=="O":
+            p1.identificacao="O"
+            p2.identificacao="X"
+        else:
+            p1.identificacao="X"
+            p2.identificacao="O"
+
     def salva_linhas(self):
         linha=randint(0,3)
         coluna=randint(0,3)
-        while self.linhas[linha-1][coluna-1]!="_":
+        while Tabuleiro.linhas[linha-1][coluna-1]!="_":
             linha=randint(0,3)
             coluna=randint(0,3)
-        self.linhas[linha-1][coluna-1]=self.identificacao
+        Tabuleiro.linhas[linha-1][coluna-1]=self.identificacao
 
-
-modo_de_jogo=input("Insira '1' se deseja jogar contra o computador, ou '2' se deseja jogar contra um amigo: ")
+p1=Humano()
+modo_de_jogo=input("Insira '1' se deseja jogar solo, ou '2' se deseja jogar contra outra pessoa: ")
 if modo_de_jogo=="1":
     p2=Computador()
 elif modo_de_jogo=="2":
-    p2=Jogador()
+    p2=Humano()
 
-p1=Jogador()
-Jogador.escolha()
+p2.identificiacao()
 continuar_o_jogo='S'
 system('cls')
 
@@ -115,26 +128,26 @@ while continuar_o_jogo=='S' or continuar_o_jogo=="s":
     while True:
         system('cls')
         print("Sua vez de jogar, jogador1!")
-        TicTacToe.imprime_tabuleiro()
+        Tabuleiro.imprime_tabuleiro()
         p1.salva_linhas()
-        TicTacToe.salva_colunas()
-        TicTacToe.salva_diagonais()
-        if TicTacToe.checar_vitoria()!="next!":
-            p1.registrar_vitoria()
+        Tabuleiro.salva_colunas()
+        Tabuleiro.salva_diagonais()
+        if Tabuleiro.checa_o_tabuleiro()!="next!":
+            p1.registra_vitoria()
             break
         system('cls')
-        if TicTacToe.checar_empate():
-            p1.registrar_empate()
+        if Jogador.checa_empate():
+            p1.registra_empate()
             break
 
         print("Sua vez de jogar, jogador2!")
-        TicTacToe.imprime_tabuleiro()
+        Tabuleiro.imprime_tabuleiro()
         p2.salva_linhas()
-        TicTacToe.salva_colunas()
-        TicTacToe.salva_diagonais()
-        if TicTacToe.checar_vitoria()!="next!":
-            p2.registrar_vitoria()
+        Tabuleiro.salva_colunas()
+        Tabuleiro.salva_diagonais()
+        if Tabuleiro.checa_o_tabuleiro()!="next!":
+            p2.registra_vitoria()
             break
     
     continuar_o_jogo=input("Deseja continuar o jogo? Insira 'S' ou 'N': ")
-    TicTacToe.limpa_tabuleiro()
+    Tabuleiro.limpa_tabuleiro()
