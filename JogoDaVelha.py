ede43ff2e90294be9c6e7f8bda436fdc0036dfa4
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2022-02-25 18:25:15 -0300

    Refatoração da TurtleRace e do Jogo da Velha (+ adição de docstrings)

--- JogoDaVelha.py
+++ JogoDaVelha.py
@@ -5,14 +5,15 @@ from dataclasses import dataclass
 
 @dataclass
 class Tabuleiro:
-    O=["O", "O", "O"]
-    X=["X", "X", "X"]
+    O = ["O", "O", "O"]
+    X = ["X", "X", "X"]
     linhas = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
     colunas = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
     diagonais = [["_", "_", "_"], ["_", "_", "_"]]
 
     @classmethod
     def imprime_tabuleiro(cls):
+        """Mostra todas as casas do tabuleiro e sua situação atual"""
         print("\n")
         for linha in cls.linhas:
             print(linha)
@@ -20,6 +21,7 @@ class Tabuleiro:
 
     @classmethod
     def checa_o_tabuleiro(cls):
+        """Checa se até o momento há algum padrão de 'X' ou 'O' que indique vitória. Caso o padrão seja encontrado, retorna o caractere correspondente à vitória; se não, retorna 'next!'."""
         if (cls.O in cls.linhas or cls.O in cls.colunas or cls.O in cls.diagonais):
             return "O"
         elif (cls.X in cls.linhas or cls.X in cls.colunas or cls.X in cls.diagonais):
@@ -29,59 +31,66 @@ class Tabuleiro:
 
     @classmethod
     def limpa_tabuleiro(cls):
-        cls.linhas=[["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-        cls.colunas=[["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
-        cls.diagonais=[["_", "_", "_"], ["_", "_", "_"]]
+        """Substitui todas as posições no tabuleiro por '_'."""
+        cls.linhas = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
+        cls.colunas = [["_", "_", "_"], ["_", "_", "_"], ["_", "_", "_"]]
+        cls.diagonais = [["_", "_", "_"], ["_", "_", "_"]]
 
 
 class Jogador:
     def __init__(self):
-        self.identificacao=""
-        self.qtd_de_vitorias=0
+        self.identificacao = ""
+        self.qtd_de_vitorias = 0
        
     def quantas_vitorias(self):
+        """Retorna a quantidade de vitórias do jogador."""
         return self.qtd_de_vitorias
 
     def checa_empate():
+        """Retorna True caso não seja encontrada mais nenhum '_' no tabuleiro."""
         if "_" not in Tabuleiro.linhas[0] and "_" not in Tabuleiro.linhas[1] and "_" not in Tabuleiro.linhas[2]:
             return True
 
     def registra_empate(self):
-            print(f"Empate, o placar está {p1.quantas_vitorias()} para o jogador 1 e {p2.quantas_vitorias()} para o jogador 2")
+        """Não registra vitória a nenhum dos jogadores e retorna o placar atual."""
+        print(f"Empate, o placar está {p1.quantas_vitorias()} para o jogador 1 e {p2.quantas_vitorias()} para o jogador 2")
     
     def registra_vitoria(self):
-        if Tabuleiro.checa_o_tabuleiro()=="X":
-            if p1.identificacao=="X":
-                p1.qtd_de_vitorias+=1
+        """Acrescenta em um ponto a quantidade de vitórias para o jogador que venceu."""
+        if Tabuleiro.checa_o_tabuleiro() == "X":
+            if p1.identificacao == "X":
+                p1.qtd_de_vitorias += 1
             else:
-                p2.qtd_de_vitorias+=1
-        if Tabuleiro.checa_o_tabuleiro()=="O":
-            if p1.identificacao=="O":
-                p1.qtd_de_vitorias+=1
+                p2.qtd_de_vitorias += 1
+        if Tabuleiro.checa_o_tabuleiro() == "O":
+            if p1.identificacao == "O":
+                p1.qtd_de_vitorias += 1
             else:
-                p2.qtd_de_vitorias+=1
+                p2.qtd_de_vitorias += 1
         print(f"Parabéns, o placar está {p1.quantas_vitorias()} para o jogador 1 e {p2.quantas_vitorias()} para o jogador 2") 
 
 
 class Humano(Jogador):
     @staticmethod
-    def identificiacao():
-        escolha=input("O jogador 1 será O ou X? ")
-        if escolha=="O":
-            p1.identificacao="O"
-            p2.identificacao="X"
+    def identificar_jogador():
+        """Salva a escolha de jogo ao jogador 1 e automaticamente salva a outra opção para o segundo jogador."""
+        escolha = input("O jogador 1 será O ou X? ")
+        if escolha == "O":
+            p1.identificacao = "O"
+            p2.identificacao = "X"
         else:
-            p1.identificacao="X"
-            p2.identificacao="O"
+            p1.identificacao = "X"
+            p2.identificacao = "O"
 
     def salva_elemento(self):
-        linha=int(input("Digite em qual linha deseja jogar: "))
-        coluna=int(input("Digite em qual coluna deseja jogar: "))
-        while Tabuleiro.linhas[linha-1][coluna-1]!="_":
+        """Lê a posição desejada pelo jogador e salva 'O' ou 'X' (a depender do jogador) na posição escolhida."""
+        linha = int(input("Digite em qual linha deseja jogar: "))
+        coluna = int(input("Digite em qual coluna deseja jogar: "))
+        while Tabuleiro.linhas[linha-1][coluna-1] != "_":
             print("A posição já está preenchida. Por favor, selecione casa desocupada")
-            linha=int(input("Digite em qual linha deseja jogar: "))
-            coluna=int(input("Digite em qual coluna deseja jogar: "))
-        Tabuleiro.linhas[linha-1][coluna-1]=self.identificacao
+            linha = int(input("Digite em qual linha deseja jogar: "))
+            coluna = int(input("Digite em qual coluna deseja jogar: "))
+        Tabuleiro.linhas[linha-1][coluna-1] = self.identificacao
         Tabuleiro.colunas.append([Tabuleiro.linhas[0][0], Tabuleiro.linhas[1][0], Tabuleiro.linhas[2][0]])
         Tabuleiro.colunas.append([Tabuleiro.linhas[0][1], Tabuleiro.linhas[1][1], Tabuleiro.linhas[2][1]])
         Tabuleiro.colunas.append([Tabuleiro.linhas[0][2], Tabuleiro.linhas[1][2], Tabuleiro.linhas[2][2]])
@@ -90,22 +99,24 @@ class Humano(Jogador):
 
 class Computador(Jogador):
     @staticmethod
-    def identificiacao():
-        escolha=input("Deseja jogar com X ou O? ")
-        if escolha=="O":
-            p1.identificacao="O"
-            p2.identificacao="X"
+    def identificar_jogador():
+        """Salva a escolha de jogo ao jogador e automaticamente salva a outra opção para o computador."""
+        escolha = input("Deseja jogar com X ou O? ")
+        if escolha == "O":
+            p1.identificacao = "O"
+            p2.identificacao = "X"
         else:
-            p1.identificacao="X"
-            p2.identificacao="O"
+            p1.identificacao = "X"
+            p2.identificacao = "O"
 
     def salva_elemento(self):
-        linha=randint(0,2)
-        coluna=randint(0,2)
-        while Tabuleiro.linhas[linha][coluna]!="_":
-            linha=randint(0,2)
-            coluna=randint(0,2)
-        Tabuleiro.linhas[linha][coluna]=self.identificacao
+        """O computador preenche uma opção randômica no tabuleiro para jogar."""
+        linha = randint(0,2)
+        coluna = randint(0,2)
+        while Tabuleiro.linhas[linha][coluna] != "_":
+            linha = randint(0,2)
+            coluna = randint(0,2)
+        Tabuleiro.linhas[linha][coluna] = self.identificacao
         Tabuleiro.colunas.append([Tabuleiro.linhas[0][0], Tabuleiro.linhas[1][0], Tabuleiro.linhas[2][0]])
         Tabuleiro.colunas.append([Tabuleiro.linhas[0][1], Tabuleiro.linhas[1][1], Tabuleiro.linhas[2][1]])
         Tabuleiro.colunas.append([Tabuleiro.linhas[0][2], Tabuleiro.linhas[1][2], Tabuleiro.linhas[2][2]])
@@ -113,19 +124,24 @@ class Computador(Jogador):
         Tabuleiro.diagonais.append([Tabuleiro.linhas[0][2], Tabuleiro.linhas[1][1], Tabuleiro.linhas[2][0]])
 
 
-p1=Humano()
-modo_de_jogo=input("Insira '1' se deseja jogar solo, ou '2' se deseja jogar com outra pessoa: ")
-p2=Computador() if modo_de_jogo=="1" else Humano()
-p2.identificiacao()
-continuar_o_jogo='S'
+p1 = Humano()
+continuar_o_jogo = 'S'
+modo_de_jogo = input("Insira '1' se deseja jogar solo, ou '2' se deseja jogar com outra pessoa: ")
+if modo_de_jogo == "1":
+    p2 = Computador()
+    p2.identificar_jogador()
+else:
+    p2 = Humano()
+    p1.identificar_jogador()
 
-while continuar_o_jogo=='S' or continuar_o_jogo=="s":
+while continuar_o_jogo == 'S' or continuar_o_jogo == "s":
     while True:
         system('cls')
         print("Sua vez de jogar, jogador1!")
         Tabuleiro.imprime_tabuleiro()
         p1.salva_elemento()
-        if Tabuleiro.checa_o_tabuleiro()!="next!":
+        
+        if Tabuleiro.checa_o_tabuleiro() != "next!":
             system('cls')
             Tabuleiro.imprime_tabuleiro()
             p1.registra_vitoria()
@@ -135,6 +151,7 @@ while continuar_o_jogo=='S' or continuar_o_jogo=="s":
             Tabuleiro.imprime_tabuleiro()
             p1.registra_empate()
             break
+
         system('cls')
         print("Sua vez de jogar, jogador2!")
         Tabuleiro.imprime_tabuleiro()
--- TurtleRace.py
+++ TurtleRace.py
@@ -3,11 +3,13 @@ import random
 
 
 def set_turtles_colors():
+    """Set each turtle color to a random color from the rainbow colors"""
     for i in range(6):
         turtles[i].color(colors[i])
 
 
 def move_turtles_to_the_initial_positions():
+    """Move all turtles to the very left of the screen"""
     y_coord=-100
     for i in range(6):
         turtles[i].penup()
@@ -15,7 +17,8 @@ def move_turtles_to_the_initial_positions():
         y_coord+=40
 
 
-def check_winner():
+def is_there_a_winner():
+    """Returns true if any of the turtles has already moved past y position y=220"""
     for i in range(6):
         turtles[i].forward(random.randint(0, 10))
         if turtles[i].pos()[0]>=220:
@@ -24,11 +27,12 @@ def check_winner():
             else:
                 print("You lost the bet.")
             print(f"The {turtles[i].color()[0]} turtle is the winner.")
-            return False
-    return True
+            return True
+    return False
 
 
 def player_won(turtle_winner):
+    """Returns true if the turtle who has reached the final position was the player's choice"""
     if turtle_winner.color()[0] == player_bet:
         return True
     return False
@@ -38,16 +42,16 @@ screen = turtle.Screen()
 screen.setup(width=500, height=400)
 turtles = [turtle.Turtle(shape="turtle") for i in range(6)]
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
-player_bet = screen.textinput("Make you bet", "Which turtle will win the race? Enter a color: ")
+player_bet = screen.textinput("Make your bet", "Which turtle will win the race? Enter one of the rainbow colors: ")
 
 if player_bet:
-    is_race_on = True
     set_turtles_colors()
     move_turtles_to_the_initial_positions()
+    is_race_on = True
 else:
     is_race_on = False
     
 while is_race_on:
-    is_race_on = check_winner()
+    is_race_on = is_there_a_winner()
            
 screen.exitonclick()
