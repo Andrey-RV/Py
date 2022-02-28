a3559b1968e5da1982b94a965f8b756bbfe6aea4
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2022-02-28 16:11:22 -0300

    Added a line in the center of the board and changed fontname

--- PongGame/board.py
+++ PongGame/board.py
@@ -8,14 +8,30 @@ class Board(turtle.Turtle):
         self.hideturtle()
         self.left_player_score = 0
         self.right_player_score = 0
+        self.draw_central_line()
         self.score_board_setup()
 
     def score_board_setup(self):
         turtle.tracer(0)
-        self.goto(x=-80, y=200)
-        self.write(arg=f"{self.left_player_score}", align="center", font=('Courier', 70,'normal'))
-        self.goto(x=80, y=200)
-        self.write(arg=f"{self.right_player_score}", align="center", font=('Courier', 70,'normal'))
+        self.goto(x=-70, y=220)
+        self.write(arg=f"{self.left_player_score}", align="center", font=('Pong Score', 40,'normal'))
+        self.goto(x=70, y=220)
+        self.write(arg=f"{self.right_player_score}", align="center", font=('Pong Score', 40,'normal'))
+        turtle.update()
+        turtle.tracer(1)
+
+    def draw_central_line(self):
+        turtle.tracer(0)
+        self.goto(x=0, y=-285)
+        self.setheading(90)
+        self.pendown()
+        self.pensize(5)
+        for _ in range(20):
+            self.forward(15)
+            self.penup()
+            self.forward(20)
+            self.pendown()
+        self.penup()
         turtle.update()
         turtle.tracer(1)
 
@@ -23,9 +39,11 @@ class Board(turtle.Turtle):
         self.right_player_score += 1
         self.clear()
         self.score_board_setup()
+        self.draw_central_line()
     
     def increase_left_player_score(self):
         self.left_player_score += 1
         self.clear()
         self.score_board_setup()
+        self.draw_central_line()
     
