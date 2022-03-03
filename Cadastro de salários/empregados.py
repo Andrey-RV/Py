ac82b4ef8e15a51042e0fe2f7301d0bf6fffec4d
Andrey-RV <andrey.rios@discente.univasf.edu.br>
2022-03-02 21:41:33 -0300

    Implementado o Pandas para exportar os dados e alguma refatoração

--- Cadastro de salários/empregados.py
+++ Cadastro de salários/empregados.py
@@ -1,20 +1,17 @@
-meses=("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro")
+meses_do_ano=("janeiro", "fevereiro", "março", "abril", "maio", "junho", "julho", "agosto", "setembro", "outubro", "novembro", "dezembro")
 
 class Empregado:
-
     def __init__(self, nome):
         self.nome = nome
-        self.salario_base={}
+        self.salario={}
         self.qtd_horas_extras_1 = {}
-        self.qtd_horas_extras_2 = {}
         self.valor_hora_extra_1 = {}
+        self.taxa_hora_extra_1={"janeiro":1.5, "fevereiro":1.5, "março":1.5, "abril":1.5, "maio":1.5,  "junho":1.5, "julho":1.5, "agosto":1.5, "setembro":1.5, "outubro":1.5, "novembro":1.5, "dezembro":1.5,}
+        self.qtd_horas_extras_2 = {}
         self.valor_hora_extra_2 = {}
-        self.tirou_ferias = {}
+        self.taxa_hora_extra_2={"janeiro":2, "fevereiro":2, "março":2, "abril":2, "maio":2, "junho":2, "julho":2, "agosto":2, "setembro":2, "outubro":2, "novembro":2, "dezembro":2,}
         self.bonificacoes = {}
         self.valor_bonificacoes = {}
-        self.taxa_hora_extra_1={"janeiro":1.5, "fevereiro":1.5, "março":1.5, "abril":1.5, "maio":1.5,  "junho":1.5, "julho":1.5, "agosto":1.5, "setembro":1.5, "outubro":1.5, "novembro":1.5, "dezembro":1.5,}
-        self.taxa_hora_extra_2={"janeiro":2, "fevereiro":2, "março":2, "abril":2, "maio":2, "junho":2, "julho":2, "agosto":2, "setembro":2, "outubro":2, "novembro":2, "dezembro":2,}
-
 
     @staticmethod
     def registrar_num_de_funcionarios():
@@ -27,38 +24,35 @@ class Empregado:
     def sera_cadastrado(self, funcionarios, mes):
         cadastro=input(f"Deseja cadastrar os ganhos para o(a) funcionário(a) {self.nome} no mês de {mes}? 'S' ou 'N': ")
         if cadastro=='N':
-            situacao=input("Qual a situação do funcionário? Digite 'férias', 'demitido' ou 'nada' caso só deseje ignorar o cadastro esse mês: ")
-            if situacao=='férias':
-                self.tirou_ferias[mes]=True
-                return False
-            elif situacao=='demitido':
-                self.tirou_ferias[mes]=False
+            situacao=input("Qual a situação do funcionário? Digite 'demitido' ou 'nada' caso só deseje ignorar o cadastro esse mês: ")
+            if situacao=='demitido':
                 for i in range(len(funcionarios)):
                     if funcionarios[i].nome==self.nome:
                         funcionarios.pop(i)
                         break
                 return False
             else:
-                self.tirou_ferias[mes]=False
                 return False
         return True
 
     
-    def cadastrar_salario_base(self, mes):
-        if len(self.salario_base)>0:
-            escolha_salario=input(f"Deseja alterar o salário de {self.salario_base[meses[meses.index(mes)-1]]}? 'S' ou 'N': ")
+    def cadastrar_salario(self, mes):
+        if len(self.salario)>0:
+            escolha_salario=input(f"Deseja alterar o salário de {self.salario[meses_do_ano[meses_do_ano.index(mes)-1]]}? 'S' ou 'N': ")
             if escolha_salario=='S':
-                self.salario_base[mes]=float(input("Insira o novo salário: R$"))
+                self.salario[mes]=float(input("Insira o novo salário: R$"))
             else:
-                self.salario_base[mes]=self.salario_base[meses[meses.index(mes)-1]]
+                self.salario[mes]=self.salario[meses_do_ano[meses_do_ano.index(mes)-1]]
         else:
-            self.salario_base[mes]=float(input("\nQual o valor de seu salário base? R$"))
+            self.salario[mes]=float(input("\nQual o valor de seu salário base? R$"))
 
     
-    def recebeu_bonificacoes(self):
+    def recebeu_bonificacoes(self, mes):
         bonificacao=input(f"\nO funcionário(a) {self.nome} recebeu alguma bonificação esse mês? 'S' ou 'N': ")
         if bonificacao=="S":
+            self.bonificacoes[mes]=True
             return True
+        self.bonificacoes[mes]=False
         return False
 
 
@@ -67,10 +61,16 @@ class Empregado:
         self.valor_bonificacoes[mes]=extra_1
 
 
-    def fez_hora_extra(self):
+    def fez_hora_extra(self, mes):
         horas_extras=input(f"\nO funcionário(a) {self.nome} fez horas extras neste mês? 'S' ou 'N': ")
         if horas_extras=="S":
             return True
+        self.qtd_horas_extras_1[mes]=0
+        self.valor_hora_extra_1[mes]=0
+        self.taxa_hora_extra_1[mes]=0
+        self.qtd_horas_extras_2[mes]=0
+        self.valor_hora_extra_2[mes]=0
+        self.taxa_hora_extra_2[mes]=0
         return False
     
 
@@ -93,7 +93,7 @@ class Empregado:
     
 
     def cadastrar_valor_hora_extra(self, mes):
-        extra_1=int(input("\nQuantas horas extras foram feitas de segunda a sábado? "))
-        extra_2=int(input("Quantas horas extras foram feitas aos domingos ou feriados? "))
-        self.valor_hora_extra_1[mes]=extra_1*self.taxa_hora_extra_1[mes]
-        self.valor_hora_extra_2[mes]=extra_2*self.taxa_hora_extra_2[mes]
+        self.qtd_horas_extras_1[mes]=int(input("\nQuantas horas extras foram feitas de segunda a sábado? "))
+        self.qtd_horas_extras_2[mes]=int(input("Quantas horas extras foram feitas aos domingos ou feriados? "))
+        self.valor_hora_extra_1[mes]=self.qtd_horas_extras_1[mes]*self.taxa_hora_extra_1[mes]*(self.salario[mes]/220)
+        self.valor_hora_extra_2[mes]=self.qtd_horas_extras_2[mes]*self.taxa_hora_extra_2[mes]*(self.salario[mes]/220)
--- Cadastro de salários/main.py
+++ Cadastro de salários/main.py
@@ -1,34 +1,27 @@
 from os import system
-from class_empregado import Empregado, meses
-from openpyxl import Workbook, load_workbook
-import excel
+from empregados import Empregado, meses_do_ano
+from dados import cadastro_por_mes, cadastrar_os_dados
+import pandas
 
-#TODO: ajustar o salvamento (TRUE/FALSE) de férias e bonificações
-
-work_book = load_workbook("Funcionarios.xlsx")
-
-funcionarios=[]
+funcionarios = []
 
 for num in range(Empregado.registrar_num_de_funcionarios()):
     funcionarios.append(Empregado(input(f"Digite o nome do(a) {num+1}º funcionário(a): ")))
 
-excel.salvar_funcionarios_no_arquivo(work_book, funcionarios, meses)
-excel.salvar_arquivo(work_book)
-
-for mes in meses:
-    system('cls')
+for mes in meses_do_ano:
+    # system('cls')
     cadastro_mensal=input(f"\nDeseja prosseguir com o cadastro para o mês de {mes}? Insira 'S ou 'N': ")
     if cadastro_mensal=='N': continue
     for funcionario in funcionarios:
         system('cls')
         if not funcionario.sera_cadastrado(funcionarios, mes): continue
-        funcionario.cadastrar_salario_base(mes)
-        if funcionario.recebeu_bonificacoes():
+        funcionario.cadastrar_salario(mes)
+        if funcionario.recebeu_bonificacoes(mes):
             funcionario.cadastrar_bonificacoes(mes)
-        if funcionario.fez_hora_extra():
+        if funcionario.fez_hora_extra(mes):
             if funcionario.taxa_hora_extra_nao_eh_padrao():
                 funcionario.cadastrar_porcentagem_extra(mes)
             funcionario.cadastrar_valor_hora_extra(mes)
-            
-
-
+        cadastrar_os_dados(mes, funcionario, cadastro_por_mes)
+        cadastro_por_mes_DataFrame=pandas.DataFrame(cadastro_por_mes[mes])
+        cadastro_por_mes_DataFrame.to_excel("cadastro_mensal.xlsx", index=False)
